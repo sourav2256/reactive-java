a7976022fd76e17e94feac696eeee79d9ffb147d
sourav2256 <souravpal7217@gmail.com>
2023-06-29 15:49:46 +0530

    add solution

---
+++ src/main/java/org/sourav/solutions/Solution6.java
@@ -0,0 +1,29 @@
+package org.sourav.solutions;
+
+import org.sourav.ReactiveSources;
+
+import java.io.IOException;
+import java.time.Duration;
+
+public class Solution6 {
+
+
+    public static void main(String[] args) throws IOException {
+
+        // Use ReactiveSources.unresponsiveFlux() and ReactiveSources.unresponsiveMono()
+
+        // Get the value from the Mono into a String variable but give up after 5 seconds
+        ReactiveSources.unresponsiveMono()
+                .block(Duration.ofSeconds(5));
+
+        // Get the value from unresponsiveFlux into a String list but give up after 5 seconds
+        // Come back and do this when you've learnt about operators!
+        ReactiveSources.unresponsiveFlux()
+                .collectList()
+                .block(Duration.ofSeconds(5));
+
+        System.out.println("Press a key to end");
+        System.in.read();
+    }
+
+}
