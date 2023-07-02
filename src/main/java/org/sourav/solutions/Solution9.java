6e6f73b504f944a408312cdec14d1e3ffe8dd2e4
sourav2256 <souravpal7217@gmail.com>
2023-07-02 13:00:54 +0530

    add solution

--- src/main/java/org/sourav/solutions/Solution9.java
+++ src/main/java/org/sourav/solutions/Solution9.java
@@ -26,6 +26,7 @@ public class Solution9 {
         // Transform to a sequence of sums of adjacent two numbers
         ReactiveSources.intNumbersFlux()
                 .buffer(2)
+                .map(list -> list.get(0) + list.get(1))
                 .subscribe(System.out::println);
 
         System.out.println("Press a key to end");
